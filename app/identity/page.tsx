41569eb04bbefcf1cdc81f486630b2e46bbf8244
yeoheung00 <yeoheung27@gmail.com>
2023-11-19 14:01:28 +0900

    add videos

---
+++ app/identity/page.tsx
@@ -0,0 +1,7 @@
+export default function Identity() {
+  return (
+    <main className="w-full h-screen overflow-hidden bg-black">
+      <video className="w-full h-full" src='/videos/identity.mp4' controls autoPlay />
+    </main>
+  )
+}
