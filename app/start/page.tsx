c3ed1bcf694d6b8d98eed6958298fe688d1dec8e
yeoheung00 <yeoheung27@gmail.com>
2023-12-06 04:50:59 +0900

    start

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,15 +1,19 @@
 
+import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Page() {
   return (
-    <main className="w-screen h-screen flex flex-col items-center ">
+    <main className="w-screen h-screen relative ">
       <div className={styles.background} />
-      <div className="flex flex-col items-center gap-10 mt-64">
+      <div className="top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] absolute flex flex-col items-center gap-6">
         <img className="w-48 h-auto" src='/symbol.svg' alt='symbol' />
-        <p className="text-center text-2xl font-Pixeled leading-[60px]">In search of<br />the Missing Stimulus</p>
-        <p className="text-center text-3xl mb-8">Find what you want</p>
+        <p className="text-center text-2xl font-Pixeled leading-[40px]">In search of<br />the Missing Stimulus</p>
       </div>
+      <Link className="top-[80%] left-1/2 translate-x-[-50%] translate-y-[-50%] absolute font-Pixeled" href="/search">
+        <img src="/button.svg" className='w-[250px] h-auto block' />
+        <h2 className='top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] absolute font-Pixeled pb-2 text-white'>Play</h2>
+      </Link>
     </main>
   )
 }
